404351a2fa7f105c69c2bbb0275a37ee7220b82d
mattjboland <mattjamesboland@gmail.com>
2019-11-07 19:43:40 +0000

    update

--- js/script.js
+++ js/script.js
@@ -20,13 +20,21 @@ $(document).ready(function() {
 	  	$(".stream3").addClass('highlight_stream');
 	});
 
-	//removes class makeRed, adds class makeBorder on mouseenter
-	$("button").mouseenter(function(){
+	// removes class makeRed, adds class makeBorder on mouseenter
+	$("button").mouseenter(function() {
 		$(this).removeClass("makeRed").addClass("makeBorder");
 	});
 
-	$("button").mouseleave(function(){
+	$("button").mouseleave(function() {
 		$("button").removeClass("makeBorder").addClass("makeRed");
 	});
 
+	$("button").click(function() {
+		$("p").slideToggle(2000);
+	});
+
+	// hides/shows paragraphs when either button is clicked
+	$("button").click(function() {
+		$("p").hide(2000).show(2000);
+	});
 });
